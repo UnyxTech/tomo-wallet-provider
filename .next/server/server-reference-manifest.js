c77312bf551f66b370ce7aa9d23a617c1c4b66e0
vincent <vincent@tomo.inc>
2024-07-25 20:40:30 +0800

    add Tomo social wallet providers

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"z0DMWGvf4f4oP79DDjsSRCc+mKEOGGYff7nOS55uH54=\"}"
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Layout from '../app/layout';
+
+export default function Home() {
+    return (
+        <Layout>
+            <h1>Welcome to the Tomo</h1>
+        </Layout>
+    );
+}
